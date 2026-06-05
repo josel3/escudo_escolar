aded2a36cbd76f4705d94bef5111ee6708760425
magginer <magginer@protonmail.com>
2026-06-05 07:24:46 -0300

    update db config

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
-DB_USER = os.getenv("DB_USER", "escudo_user")
+DB_USER = os.getenv("DB_USERNAME", "escudo_user")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "escudo_pass")
 DB_NAME = os.getenv("DB_NAME", "escudoescolar")
 
